01ee3872011dbce1ae363dc67c3617dc40ce551e
Иван Харитонов <haritonoviwan@yandex.ru>
2023-08-24 11:56:25 +0300

    Сделал 43 задание

---
+++ ex43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 43 Напишите программу, которая найдёт
+// точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+float k_1 = GetNumberFromUser("Введите угловой коэффициент k первой прямой: ", "Ошибка ввода");
+float b_1 = GetNumberFromUser("Введите cмещение b первой прямой: ", "Ошибка ввода");
+float k_2 = GetNumberFromUser("Введите угловой коэффициент k второй прямой: ", "Ошибка ввода");
+float b_2 = GetNumberFromUser("Введите смещение b второй прямой: ", "Ошибка ввода");
+Console.WriteLine($"k1 = {k_1}, b1 = {b_1}, k2 = {k_2}, b2 = {b_2} -> ");
+if (k_1 == k_2)
+    Console.WriteLine("Прямые параллельны или совпадают, точки пересечения нет");
+else
+{
+    float[] my_intersection = FoundIntersection(k_1, b_1, k_2, b_2);
+    Console.Write(String.Join("; ", my_intersection));
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+float[] FoundIntersection(float k1, float b1, float k2, float b2)
+{
+    float[] x_y = new float[2];
+    x_y[0] = (b2 - b1) / (k1 - k2);
+    x_y[1] = k1 * x_y[0] + b1;
+    return x_y;
+}
